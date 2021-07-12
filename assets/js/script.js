c8f8f7baeff46bb9a338040920d4c5f410cf249b
BesnikShala <bes.shala@gmail.com>
2021-07-12 19:52:04 +0000

    added scroll function to change nav background color for better visibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// change navbar background
+
+function activeNav () {
+    let navbar = document.getElementById('navbarNav');
+    let scrollValue = window.scrollY;
+
+    if(scrollValue < 350) {
+        navbar.classList.remove('active-nav')
+    }  else {
+        navbar.classList.add('active-nav')
+    }
+}
+
+window.addEventListener('scroll', activeNav);
+
+
+// Change content of places page via hide/show function
+
 function showInfo(ab) {
     if(ab == 0){
         document.getElementsByClassName('info')[0].style.display="block";
