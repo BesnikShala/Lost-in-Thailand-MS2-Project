8d44bcedebd3dc5102dac713306a406d71d58893
BesnikShala <bes.shala@gmail.com>
2021-07-20 14:53:23 +0000

    updated script.js included marker for temple and added info window with text and link

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,33 +14,14 @@ function activeNav () {
 window.addEventListener('scroll', activeNav);
 
 
-// Change content of places page via hide/show function
-
-// function showInfo(ab) {
-//     if(ab == 0){
-//         document.getElementsByClassName('info')[0].style.display="block";
-//         document.getElementsByClassName('info')[1].style.display = "none";
-//         document.getElementsByClassName('info')[2].style.display = "none";
-        
-//     }
-//     if (ab == 1){
-//         document.getElementsByClassName('info')[0].style.display = "none";
-//         document.getElementsByClassName('info')[1].style.display = "block";
-//         document.getElementsByClassName('info')[2].style.display = "none";
-//     }
-//      if (ab == 2) {
-//          document.getElementsByClassName('info')[0].style.display = "none";
-//          document.getElementsByClassName('info')[1].style.display = "none";
-//          document.getElementsByClassName('info')[2].style.display = "block";
-//      }
-// }
+// Change content of places page via for loop function
 
 function showInfo(index) {
     const infoSections = document.getElementsByClassName("info")
     for (i = 0; i < infoSections.length; i++) {
         infoSections[i].style.display = "none";
     }
-    infoSections[index].style.display = "block";
+    infoSections[index].style.display = "flex";
 };
 
 // Google maps function
@@ -61,5 +42,43 @@ function initMap() {
             lng: 100.5018
         },
         map: map
-    })
-}
+    });
+
+    var markerString = '<div id="content">' + 
+                     '<div id="contentHeader">' + 
+                     '<h3 class="contentTitle">The Sanctuary of Truth | Pattaya</h3>' + 
+                     '</div>' +
+                     '<div id="mainContent">' +
+                     '<p>Just a few hours drive or taxi from <b>Bangkok</b> this is by far the most recommended location to visit' + 
+                     'if you are looking for amazing archtiecture. This temple is carved from various types of wood such as <b>Teak</b>' +
+                     '<b>Mai Deang</b>, <b>Mai Takien</b> and <b>Mai Panchaat</b>. This is all hand carved and there is not one inch of the' + 
+                     'temple which has not been carved and detailed. Building started in 1981 and still ongoing, the wood is replaced once it' +
+                     'gets weathered and damaged over time. It is continupusly replensihed and you can see the carving on site and even join in. </p>' +
+                     '<p>See website for more details : <a href="http://www.bangkok.com/pattaya/attractions/the-sanctuary.htm"></a></p>' +
+                     '</div>' + 
+                     '</div>';
+
+   
+
+    const infowindow = new google.maps.InfoWindow({
+        content: markerString,
+    });
+
+    const markers = new google.maps.Marker({
+        position: {
+            lat: 12.9727,
+            lng: 100.889
+        },
+        content: markerString,
+        map,
+        title: "Sanctuary of Truth",
+    });
+
+    markers.addListener("click", () => {
+        infowindow.open({
+            anchor: markers,
+            map,
+            shouldFocus: false,
+        });
+    });
+};
