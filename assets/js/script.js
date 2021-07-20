fb43efa388688e4ad99a6f68a5976b2ad7aece07
BesnikShala <bes.shala@gmail.com>
2021-07-20 16:18:20 +0000

    changed js code to house multiple info windows with large content

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,25 +16,6 @@ window.addEventListener('scroll', activeNav);
 
 // Change content of places page via for loop function
 
-// function showInfo(ab) {
-//     if(ab == 0){
-//         document.getElementsByClassName('info')[0].style.display="block";
-//         document.getElementsByClassName('info')[1].style.display = "none";
-//         document.getElementsByClassName('info')[2].style.display = "none";
-
-//     }
-//     if (ab == 1){
-//         document.getElementsByClassName('info')[0].style.display = "none";
-//         document.getElementsByClassName('info')[1].style.display = "block";
-//         document.getElementsByClassName('info')[2].style.display = "none";
-//     }
-//      if (ab == 2) {
-//          document.getElementsByClassName('info')[0].style.display = "none";
-//          document.getElementsByClassName('info')[1].style.display = "none";
-//          document.getElementsByClassName('info')[2].style.display = "block";
-//      }
-// }
-
 function showInfo(index) {
     const infoSections = document.getElementsByClassName("info")
     for (i = 0; i < infoSections.length; i++) {
@@ -54,50 +35,72 @@ function initMap() {
         }
     });
 
-    const marker = new google.maps.Marker({
-        position: 
+    var markers = [
         {
-            lat: 13.7563,
-            lng: 100.5018
+         coords: {
+            lat: 12.9727, 
+            lng: 100.889
         },
-        map: map
-    });
+         content: '<div id="content">' +
+             '<div id="contentHeader">' +
+             '<h3 class="contentTitle">The Sanctuary of Truth | Pattaya</h3>' +
+             '</div>' +
+             '<div id="mainContent">' +
+             '<p>Just a few hours drive or taxi from <b>Bangkok</b> this is by far the most recommended location to visit' +
+             'if you are looking for amazing archtiecture. This temple is carved from various types of wood such as <b>Teak</b>' +
+             '<b>Mai Deang</b>, <b>Mai Takien</b> and <b>Mai Panchaat</b>. This is all hand carved and there is not one inch of the' +
+             'temple which has not been carved and detailed. Building started in 1981 and still ongoing, the wood is replaced once it' +
+             'gets weathered and damaged over time. It is continupusly replensihed and you can see the carving on site and even join in. </p>' +
+             '<p>See website for more details : <a href="http://www.bangkok.com/pattaya/attractions/the-sanctuary.htm" target="_blank">Sanctuary of Truth</a></p>' +
+             '</div>' +
+             '</div>',
+        },
+        {
+        coords: {
+            lat: 13.7516,
+            lng: 100.4927
+        },
+            content: '<div id="content">' +
+            '<div id="contentHeader">' +
+            '<h3 class="contentTitle">The Emerald Buddha | Bangkok</h3>' +
+            '</div>' +
+            '<div id="mainContent">' +
+            '<p>Officially called Wat Phra Kaew, this temple is located in <b>Bangkok</b> and is regarded as the most sacred buddhist temples.' +
+            'The temple is located within the Grand Palace which holds many historic buildings within. The statue is 66 centimeters tall and' +
+            ' made of a stone called <b>Jasper</b> it is green which has given it the name of Emerald Buddah. The statue is also clothed in <b>gold</b>' +
+            'which changes during the seasons. This is truly amazing to see and is also recommended to go and see, the statue is dated back to 1434' +
+            'and is a real thai tresure. It is used for ceremonies up to three times a year.</p>' +
+            '<p>See website for more details : <a href="https://www.buddhistdoor.net/features/tales-of-the-emerald-buddha-simplicity-and-splendor" target="_blank">The emerald Buddha</a></p>' +
+            '</div>' +
+            '</div>',
+        }
+    ];
 
-    var markerString = '<div id="content">' + 
-                     '<div id="contentHeader">' + 
-                     '<h3 class="contentTitle">The Sanctuary of Truth | Pattaya</h3>' + 
-                     '</div>' +
-                     '<div id="mainContent">' +
-                     '<p>Just a few hours drive or taxi from <b>Bangkok</b> this is by far the most recommended location to visit' + 
-                     'if you are looking for amazing archtiecture. This temple is carved from various types of wood such as <b>Teak</b>' +
-                     '<b>Mai Deang</b>, <b>Mai Takien</b> and <b>Mai Panchaat</b>. This is all hand carved and there is not one inch of the' + 
-                     'temple which has not been carved and detailed. Building started in 1981 and still ongoing, the wood is replaced once it' +
-                     'gets weathered and damaged over time. It is continupusly replensihed and you can see the carving on site and even join in. </p>' +
-                     '<p>See website for more details : <a href="http://www.bangkok.com/pattaya/attractions/the-sanctuary.htm" target="_blank">Sanctuary of Truth</a></p>' +
-                     '</div>' + 
-                     '</div>';
+    
+
+  
 
    
 
-    const infowindow = new google.maps.InfoWindow({
-        content: markerString,
-    });
+    // const infowindow = new google.maps.InfoWindow({
+    //     content: markerString,
+    // });
 
-    const markers = new google.maps.Marker({
-        position: {
-            lat: 12.9727,
-            lng: 100.889
-        },
-        content: markerString,
-        map,
-        title: "Sanctuary of Truth",
-    });
+    // const markers = new google.maps.Marker({
+    //     position: {
+    //         lat: 12.9727,
+    //         lng: 100.889
+    //     },
+    //     content: markerString,
+    //     map,
+    //     title: "Sanctuary of Truth",
+    // });
 
-    markers.addListener("click", () => {
-        infowindow.open({
-            anchor: markers,
-            map,
-            shouldFocus: false,
-        });
-    });
+    // markers.addListener("click", () => {
+    //     infowindow.open({
+    //         anchor: markers,
+    //         map,
+    //         shouldFocus: false,
+    //     });
+    // });
 };
