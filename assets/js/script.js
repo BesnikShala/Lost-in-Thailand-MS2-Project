b169dd250600a33ea8d797521be207a81ef03cf9
BesnikShala <bes.shala@gmail.com>
2021-07-21 00:33:35 +0000

    updated maps section included additional markers of locations and included information in the info window

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,6 +73,97 @@ function initMap() {
             '<p>See website for more details : <a href="https://www.buddhistdoor.net/features/tales-of-the-emerald-buddha-simplicity-and-splendor" target="_blank">The emerald Buddha</a></p>' +
             '</div>' +
             '</div>',
+        },
+        {
+        coords: {
+                lat: 13.7437,
+                lng: 100.4889
+            },
+            content: '<div id="content">' +
+            '<div id="contentHeader">' +
+            '<h3 class="contentTitle">Temple of Dawn | Bangkok</h3>' +
+            '</div>' +
+            '<div id="mainContent">' +
+            '<p>Officially called Wat Arun, this temple is also located in <b>Bangkok</b> on the banks of the Chao Phraya River.' +
+            'The temple is dedicated to the hindu god Aruna, the recommended time to see this is during sun rise or sun set as the light reflects from the' +
+            ' temple wals. The temple has a 70 meter spire which is one of Bangkoks most famous landmarks. We recommend visiting this temple via a water taxi' +
+            ' which can be taken from various points along the river banks. You can also climb to the top of the temple spire via very steep stairs which gives' +
+            ' a very rewarding view of the Chao Phraya River.</p>' +
+            '<p>See website for more details : <a href="https://uk.hotels.com/go/thailand/wat-arun" target="_blank">The Temple of Dawn</a></p>' +
+            '</div>' +
+            '</div>',
+        },
+        {
+            coords: {
+                lat: 13.7667,
+                lng: 100.5141
+            },
+            content: '<div id="content">' +
+                '<div id="contentHeader">' +
+                '<h3 class="contentTitle">The Marble Temple | Bangkok</h3>' +
+                '</div>' +
+                '<div id="mainContent">' +
+                '<p>Officially called Wat Benjamabhopit, this temple is also located in <b>Bangkok</b> not too far from the Grand Palace.' +
+                'The temple is well known and has earned the nickname of the Marble Temple as the exterior walls are covered in marble imported from Italy' +
+                ' The temple is recommended for a visit as it is a very significant temple to thailand and appears on the thai currency. It was built in early 1900s ' +
+                'by King Rama V and is regarded as one of the most important royal temples as itt houses the ashes of the king himself.' +
+                ' </p>' +
+                '<p>See website for more details : <a href="https://en.wikipedia.org/wiki/Wat_Benchamabophit" target="_blank">The Marble Temple</a></p>' +
+                '</div>' +
+                '</div>',
+        },
+        {
+            coords: {
+                lat: 13.7627,
+                lng: 100.5027
+            },
+            content: '<div id="content">' +
+                '<div id="contentHeader">' +
+                '<h3 class="contentTitle">Wat Intharawihan | Bangkok</h3>' +
+                '</div>' +
+                '<div id="mainContent">' +
+                '<p>This temple is also located in <b>Bangkok</b> in the Phra Nakhon district. It is known for the 105 ft buddha statue which is must see' +
+                ' site. The temple is easily accessible via the river with a water taxi on the same river as the Wat Arun temple. We recommend this way of visiting' +
+                ' the temple as you get to see the best view of the statue from a far distance. As you gte close you begin to realise how big the statue is truly.</p>' +
+                '<p>See website for more details : <a href="https://en.wikipedia.org/wiki/Wat_Benchamabophit" target="_blank">The Marble Temple</a></p>' +
+                '</div>' +
+                '</div>',
+        },
+        {
+            coords: {
+                lat: 7.8760,
+                lng: 98.2762
+            }, 
+            content: '<div id="content">' +
+                '<div id="contentHeader">' +
+                '<h3 class="contentTitle"> Freedom Beach | Phuket</h3>' +
+                '</div>' +
+                '<div id="mainContent">' +
+                '<p>Freedom Beach is located on the central east coast of Phuket. It has 300 meters of the finest white sand beach, this little paradise' +
+                ' is highly recommended but is tricky to get to. The trip is worth it however, you will either need to take the traditional fishtail boat or ' +
+                ' you can navigate the steep footpath through the mountainous terrain. The beach is not very commercial so if you want a getaway and peacful ' +
+                'taste of paradise we highly recommend this beach.</p>' +
+                '<p>See website for more details : <a href="https://www.phuket101.net/freedom-beach/" target="_blank">Freedom Beach</a></p>' +
+                '</div>' +
+                '</div>',
+        },
+        {
+            coords: {
+                lat: 7.8491,
+                lng: 98.2930
+            }, 
+            content: '<div id="content">' +
+                '<div id="contentHeader">' +
+                '<h3 class="contentTitle"> Karon Beach | Phuket</h3>' +  // to finish
+                '</div>' +
+                '<div id="mainContent">' +
+                '<p>Karon Beach is located on the central east coast of Phuket. It has 300 meters of the finest white sand beach, this little paradise' +
+                ' is highly recommended but is tricky to get to. The trip is worth it however, you will either need to take the traditional fishtail boat or ' +
+                ' you can navigate the steep footpath through the mountainous terrain. The beach is not very commercial so if you want a getaway and peacful ' +
+                'taste of paradise we highly recommend this beach.</p>' +
+                '<p>See website for more details : <a href="https://www.phuket101.net/freedom-beach/" target="_blank">Freedom Beach</a></p>' +
+                '</div>' +
+                '</div>',
         }
     ];
 
